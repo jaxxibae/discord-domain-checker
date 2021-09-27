8dd266e182fb3b67d07ab16343f413fee6447980
David Paiva <pillgp@icloud.com>
2021-09-27 15:58:39 +0100

    First commit!

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import domains from './domains'
+import { Component } from 'react';
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = { domain: '', status: '', isValid: null }
+
+    this.updateInput = this.updateInput.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }  
+
+  updateInput (event) {
+    this.setState({ domain: event.target.value })
+  }
+
+  handleSubmit (event) {
+    const domain = this.state.domain
+
+    if (!domain) return;
+
+    const strippedDomain = this.domainFromUrl(domain)
+
+    if (domains.includes(strippedDomain)) {
+      this.setState({ status: `${strippedDomain} is a valid Discord domain.`, isValid: true })
+    } else {
+      this.setState({ status: `${strippedDomain} isn't a valid Discord domain.`, isValid: false })
+    }
+  }
+
+  domainFromUrl (url) {
+    var result
+    var match
+    if (match = url.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n\?\=]+)/im)) {
+        result = match[1]
+        if (match = result.match(/^[^\.]+\.(.+\..+)$/)) {
+            result = match[1]
+        }
+    }
+    return result.toLowerCase()
+}
+
+  render () {
+    return (
+      <div className="App">
+      <header className="App-header">
+        <h1>
+          Discord Domain Checker
+        </h1>
+        <p>
+          Do you doubt of a website that says its made by Discord? Submit it here:
+        </p>
+        <input className="input" type="text" name="domain" placeholder="discord.com" onChange={this.updateInput} />
+        <h3 className={this.state.isValid?.toString()}>{(this.state && this.state.status) ? this.state.status : ''}</h3>
+        <Button variant="primary" onClick={this.handleSubmit}>Submit</Button>
+      </header>
+    </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/domains.js
@@ -0,0 +1,10 @@
+const domains = [
+  "discord.com",
+  "discordapp.com",
+  "discord.store",
+  "discord.gift",
+  "discord.gg",
+  "discordapp.net"
+]
+
+export default domains
